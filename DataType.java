153100ed28f8835266f1fdea4e8ba35ac08304ed
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-11 11:28:41 +0530

    Add DataType.java

---
+++ DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+    public static void main(String[] args) {
+        int age=10;
+        float pi=3.14f;
+        char grade = 'A';
+        double d=22.45;
+        boolean isPass=true;
+        short f=3200;
+        long l=123456789L;
+        byte b=100;
+
+        System.out.println(age);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(isPass);
+        System.out.println(b);
+        
+        
+
+
+
+    }
+}
