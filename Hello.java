2bb1e0a3f3b1911bb7c2d45e6301c73979e77446
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-11 09:56:40 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args){
+        // This is my first program
+        System.out.println("ISHAAN VARSHNEY ");
+        System.out.println("HELLO");
+        /*After class this post 
+        on git hub*/
+    }
+}
