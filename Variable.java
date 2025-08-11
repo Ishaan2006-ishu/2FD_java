aa4e132407936b7535e196606a5dc7b380ac5933
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-11 11:07:52 +0530

    Added Variable.java

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+        int c=10;
+        int ishaanVarshney=20;
+        int _ishu=30;
+        int $ishu=40;
+        System.out.println(c);
+        System.out.println(ishaanVarshney);
+        System.out.println(_ishu);
+        System.out.println($ishu);
+
+        int a=20;//local varible
+        int b=30;//local variable
+        System.out.println("Sum=" + (a+b));
+
+
+
+
+
+
+    }
+}
