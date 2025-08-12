e70b20a3f395136ad6883bff7f7997c9554ebc51
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-12 09:19:20 +0530

    Add User_input.java

---
+++ User_input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class User_input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a no:-");
+        int a=sc.nextInt();
+        System.out.println("Enter a no:-");
+        int b=sc.nextInt();
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("a+b="+(a+b));
+
+    }
+}
