9f75c98840d793aee4c03987b7bd8d28c181e611
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-12 08:39:56 +0530

    Add non_pri.java

---
+++ non_pri.java
@@ -0,0 +1,15 @@
+public class non_pri {
+    public static void main(String[] args) {
+        String s="ishaan";
+        
+        
+        int []arr={1,2,3,4,5,6};
+        System.out.println(arr[3]);
+        System.out.println(s);
+
+        //Mutiple declaration
+        int a=10, b=20,c=30;
+        System.out.println(a+" "+b+" "+c);
+
+    }
+}
