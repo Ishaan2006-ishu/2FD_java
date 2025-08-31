4f354cda7c5f125c35c4bfa775df8f2b9353734e
Ishaan2006-ishu <vars.ishaan2006@gmail.com>
2025-08-31 19:29:02 +0530

    some updation

--- Assignment_01/q4.java
+++ Assignment_01/q4.java
@@ -11,12 +11,9 @@ public class q4 {
         Scanner sc=new Scanner(System.in);
             int N=sc.nextInt();
             String str=""+N;
-            int count=0;
-            for (int i= 0; i < str.length(); i++) {
-                
-                count+=1;
-            }
-            System.out.println("Number of digits="+count);
+            
+            
+            System.out.println("Number of digits="+str.length());
 
 
 
